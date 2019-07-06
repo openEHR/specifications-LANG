6470b54613390395c57719acfa59537c68c63f58
wolandscat <thomas.beale@openehr.org>
2019-07-06 12:42:57 +0100

    Update publishing scripts.

--- do_uml_generate-spec-LANG.sh
+++ do_uml_generate-spec-LANG.sh
@@ -1,4 +1,4 @@
-rm docs/UML/classes/*.*
-rm docs/UML/diagrams/*.*
+rm -f docs/UML/classes/*.*
+rm -f docs/UML/diagrams/*.*
 
-./uml_generate.sh -i {lang_release} -r LANG -o docs/UML computable/UML/openEHR_UML-LANG.mdzip
+../specifications-AA_GLOBAL/bin/uml_generate.sh -i {lang_release} -r LANG -o docs/UML computable/UML/openEHR_UML-LANG.mdzip
