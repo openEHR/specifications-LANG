04a6b29420c029bece1d187ecebd7ebc6b94be37
wolandscat <thomas.beale@openehr.org>
2020-03-07 18:35:36 +0000

    UML cleanup and remove regenerate script.

--- do_uml_generate-spec-LANG.sh
+++
@@ -1,4 +0,0 @@
-rm -f docs/UML/classes/*.*
-rm -f docs/UML/diagrams/*.*
-
-../specifications-AA_GLOBAL/bin/uml_generate.sh -d svg -i {lang_release} -r LANG -o docs/UML computable/UML/openEHR_UML-LANG.mdzip
